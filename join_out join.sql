d9fc94e6661790825b176543c91fcc7dd27e8919
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-02-13 15:29:51 -0700

    Update join_out join.sql

--- join_out join.sql
+++ join_out join.sql
@@ -1,4 +1,6 @@
 - To get both customers who placed orders and customers who didn’t place orders in the result, you need to use an outer join, like this:
+- But to make sure that the NULL is a placeholder for an outer row and not a NULL that originated from the table, 
+- it’s recommended that you refer to an attribute that is the primary key, or the join column, or one defined as not allowing NULLs.
 
 
 SELECT C.custid, C.companyname, O.orderid, O.orderdate
