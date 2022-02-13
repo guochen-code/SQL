9d9c35ca92ca1d876162ef58d2d7582f35241243
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-02-13 00:02:57 -0700

    Update join_out join.sql

--- join_out join.sql
+++ join_out join.sql
@@ -6,4 +6,5 @@ FROM Sales.Customers AS C
   LEFT OUTER JOIN Sales.Orders AS O
     ON O.custid = C.custid;
 
-- This query returns 832 rows (including the customers 22 and 57, who didn’t place orders). An inner join between the tables would return only 830 rows, without those customers.
+- This query returns 832 rows (including the customers 22 and 57, who didn’t place orders). An inner join between the tables would return only 830 rows, 
+- without those customers.
