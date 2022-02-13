6d230f8e174c7434e6697889a86db4af6dd54d33
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-02-12 23:28:20 -0700

    Update case.sql

--- case.sql
+++ case.sql
@@ -17,3 +17,13 @@ SELECT empid, firstname, lastname, titleofcourtesy,
     ELSE                                        'Unknown'
   END AS gender
 FROM HR.Employees;
+
+
+- By default, SQL Server sorts NULLs before non-NULL values. To get NULLs to sort last, you can use a CASE expression that returns 1 when the region column is NULL 
+- and 0 when it is not NULL. Specify this CASE expression as the first sort column and the region column as the second. 
+- This way, non-NULLs sort correctly among themselves first followed by NULLs.
+
+SELECT custid, region
+FROM Sales.Customers
+ORDER BY
+  CASE WHEN region IS NULL THEN 1 ELSE 0 END, region;
