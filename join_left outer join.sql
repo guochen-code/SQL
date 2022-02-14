0d2631a31a2bb00aca4f67514c4f64d36ee978e9
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-02-14 09:09:26 -0700

    Update join_left outer join.sql

--- join_left outer join.sql
+++ join_left outer join.sql
@@ -48,6 +48,22 @@ WHERE O.orderdate = '20160212'
 - Then the WHERE filter keeps only rows where the order date is February 12, 2016 or the order ID is NULL (a customer without orders at all). 
 - The filter discards customers who didn’t place orders on February 12, 2016 but did place orders on other dates, 
 - and according to Exercise 7 the query is supposed to return all customers. 
+- Observe that many of the 91 customers from the Customers table are missing. As mentioned, those are customers who didn’t place orders on February 12, 2016, 
+- but did place orders on other dates.
 
- - Observe that many of the 91 customers from the Customers table are missing. As mentioned, those are customers who didn’t place orders on February 12, 2016, 
- - but did place orders on other dates.
+
+#####
+- Return all customers, and for each return a Yes/No value depending on whether the customer placed orders on February 12, 2016:
+- Tables involved: Sales.Customers and Sales.Orders
+
+SELECT DISTINCT C.custid, C.companyname,
+  CASE WHEN O.orderid IS NOT NULL THEN 'Yes' ELSE 'No' END AS HasOrderOn20160212
+FROM Sales.Customers AS C
+  LEFT OUTER JOIN Sales.Orders AS O
+    ON O.custid = C.custid
+    AND O.orderdate = '20160212';
+    
+- Remember that in an outer join, a nonmatch is identified as an outer row with NULLs in the attributes of the nonpreserved side. 
+- So you can use a simple CASE expression that checks whether the current row is not an outer one, in which case it returns Yes; otherwise, it returns No. 
+- Because technically you can have more than one match per customer, you should add a DISTINCT clause to the SELECT list.
+ 
