c60877b07c0af01ef8e4b46d0c665ecba4483a37
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-02-16 00:31:54 -0700

    Create top(n) with ties.sql

---
+++ top(n) with ties.sql
@@ -0,0 +1,10 @@
+- Write a query that returns all orders placed by the customer(s) who placed the highest number of orders. 
+- Note that more than one customer might have the same number of orders:
+
+SELECT custid, orderid, orderdate, empid
+FROM Sales.Orders
+WHERE custid IN
+  (SELECT TOP (1) WITH TIES O.custid
+   FROM Sales.Orders AS O
+   GROUP BY O.custid
+   ORDER BY COUNT(*) DESC);
